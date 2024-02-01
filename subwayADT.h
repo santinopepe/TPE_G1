17c8e3b7f724580cbe1ca07a230b0b5f589e334a
santinopepe <spepe@itba.edu.ar>
2024-02-01 19:36:09 +0100

    Comentarios sobre funciones

--- subwayADT.h
+++ subwayADT.h
@@ -5,8 +5,11 @@ typedef struct subCDT * subADT;
 
 subADT newSub(void);
 
+// For the correct use of de TAD, you need to first use the function createStations, this will then enable you
+// to use the function addData.
 void createStations(subADT sub, char line, char * name, size_t stationID);
 
+// This function adds the data that later will be processed.
 void addData(subADT sub, char day, char month, size_t year, size_t stationID, size_t cantPassen, char start, char end);
 
 void freeSub(subADT sub);
