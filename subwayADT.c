8e3a0a5e3f3d3ec9eec6d2a5ff3f88830a2be0ac
maraujofeltrin <manuel.araujofeltrin@gmail.com>
2024-02-01 14:54:37 -0300

    actualizacion struct
    Co-Authored-By: catatraj <71651338+catatraj@users.noreply.github.com>
    Co-Authored-By: santinopepe <146956051+santinopepe@users.noreply.github.com>

--- subwayADT.c
+++ subwayADT.c
@@ -7,19 +7,36 @@
 
 enum{MORNING=0, LUNCH, NOON, NIGHT};
 
+typedef struct info{ //PONER MEJOR NOMBRE
+    char * name;
+    size_t numPasan;
+}info;
+
+typedef struct node{
+    char * name; //para el query 1 es la linea y para el query 2 el nombre
+    size_t numTot;
+    struct node * tail;
+}Tnode;
+
+typedef struct avgTop{
+    char * name;
+    float avg;
+    size_t year;
+    char month;
+    struct avgTop * tail;
+}avgTop;
+
+typedef struct node * Tlist;
+
 typedef struct Tmonth{
     size_t totalMonth;
     int numDay;//dias que hay en el mes (debe haber una formula para sacarlo por lo cual esto no deberia ir probablemente)
 }Tmonth;
 
-typedef struct Tweek{ //quedamos con la duda si hacerlo como dice cata (consultar pepe)
-    size_t period[4]; //para cada periodo de el dia donde almacenar la cant de pasajeros en ese periodo (QUERY 3)
-}Tweek;
-
 typedef struct Tstation{
     char * name;
     size_t pasenStation; //para query 2 no va a ser lo mas eficiente pero hay que usar el id para la busqueda de mas 
-    Tweek days[7]; //dias de la semana para almacenar la cant de gente por estacion
+    size_t days[4][7]; //matriz de dias de la semana con periodos del dia
     Tmonth * historyMonth[12]; //le puse el asterisco para que busque su año y mes y ponga ahi la cantidad de gente
     size_t yearEnd;
 }Tstation;
@@ -27,12 +44,20 @@ typedef struct Tstation{
 typedef struct Tline{
     size_t pasaenTot; //pasajeros por linea
     Tstation * station; //vector donde los id de las estaciones son la posicion al estos ser mayor a 0 CREO
-    size_t numStation;
     size_t dimStation;
+    Tlist top; //QUERY 2
 }Tline;
 
-typedef struct subADT{
-    size_t numLines;
-    Tline * lines; //vector donde cada posicion es una linea de subte
+
+typedef struct subCDT{
+    char * line; //vector
+
+    Tline * lines; //vector donde cada posicion es una linea de subte y estan los results
     size_t dimLines;
+
+    Tlist list1; //QUERY 1
+
+    info days[4][7]; //QUERY 3 
+
+    avgTop list4; //QUERY 4
 };
